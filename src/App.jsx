f8ba52fc32a37b384abf30c3b980dd989c9b94b3
Ritikchandra <114809062+Ritikchandra@users.noreply.github.com>
2023-12-27 23:43:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/navbar'
 import Hero from './components/HeroSection/Hero'
 import './App.css'
 
