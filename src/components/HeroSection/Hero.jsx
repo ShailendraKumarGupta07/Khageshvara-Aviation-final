b6544f99da92df605b27ed520837547c98243e9b
Laxman Patel <f20231197@pilani.bits-pilani.ac.in>
2024-02-17 00:19:24 +0530

    fixes #2

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import styles from "./Hero.module.css"
-import bgImage from "../../assets/heroBgImage.webp"
+import React from "react";
+import styles from "./Hero.module.css";
+import bgImage from "../../assets/heroBgImage.webp";
+
 const Hero = () => {
   return (
     <>
-        <div className={styles.wrapper}>
-        <img src={bgImage} alt="" className={styles.bgImage}/>
+      <div className={styles.wrapper}>
+        <img src={bgImage} alt="" className={styles.bgImage} />
         <div className={styles.content}>
-        <div className={styles.textSection}>
-          <div className={styles.bigTextWrapper}>
-          NEXT GEN <span>MOBILITY</span> 
+          <div className={styles.textSection}>
+            <div className={styles.bigTextWrapper}>
+              NEXT GEN <span>MOBILITY</span>
+            </div>
+            <div className={styles.smallTextWrapper}>
+              Khageshvara's Groundbreaking <span>'Air Taxi'</span>, Your Express
+              Ticket to Faster, Smoother Trips.
+            </div>
           </div>
-          <div className={styles.smallTextWrapper}>
-          Khageshvara's Groundbreaking <span>'Air Taxi'</span> , Your Express Ticket to Faster, Smoother Trips.
-          </div>
-        </div>
           <div className={styles.btnWrapper}>
             <button className={styles.btn}>Join Our Revolution</button>
           </div>
         </div>
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
